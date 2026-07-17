261b5244a74d61a56dbe21af7ef15278c1156b49
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-17 21:59:55 +0530

    reversearr

--- Array/reversearray.py
+++ Array/reversearray.py
@@ -1,7 +1,10 @@
 arr=[1,2,3,4,5]
 left=0
 right=len(arr)-1
-arr1=[0]
-for i in range(right,left-1,-1):
-    arr1=arr[i]
-print(arr1)
+while left<right:
+    temp=arr[left]
+    arr[left]=arr[right]
+    arr[right]=temp
+    left=left+1
+    right=right-1
+print(arr)
