510b0aab66439f3a896b44c16b65d5a94b00c065
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-20 15:59:52 +0530

    arrayrotation

--- Array/arrayrotation.py
+++ Array/arrayrotation.py
@@ -4,10 +4,10 @@ last=len(arr)-1
 temp=arr[0]
 j=1
 for i in range(0,last):
-    if j>len(arr):
-        break
-    else:
-        arr[i]=arr[j]
-        j=j+1
+    # if j>len(arr):
+    #     break
+    # else:
+    arr[i]=arr[j]
+    j=j+1
 arr[last]=temp
 print(arr)
