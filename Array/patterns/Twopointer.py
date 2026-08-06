5f429a1e524404ede17b545aa7f1d3f007297f5d
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-06 14:33:58 +0530

    2pointers

--- Array/patterns/Twopointer.py
+++ Array/patterns/Twopointer.py
@@ -1,1 +1,12 @@
 n=[2,5,11,15]
+target=7
+left=0
+right=len(n)-1
+while left<right:
+    if (n[left]+n[right]==target):
+        print(f"{n[left]}&{n[right]}")
+        break
+    elif n[left]+n[right]>target:
+        right=right-1
+    else:
+        left=left-1
