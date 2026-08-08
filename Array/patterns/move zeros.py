ff543a4666548bab99a4ffbc7b23a88b4216c39d
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-08 20:46:54 +0530

    Move-all-zeros-to- the-end

--- Array/patterns/move zeros.py
+++ Array/patterns/move zeros.py
@@ -1,1 +1,9 @@
 nums=[0,1,0,11,23,0]
+left=0
+for right in range(0,len(nums)):
+    if nums[right]!=0:
+        temp=nums[right]
+        nums[right]=nums[left]
+        nums[left]=temp
+        left=left+1
+print(nums)
